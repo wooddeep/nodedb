45fdf34ec49e921e3d8ddd6360da4fd587f48865
lihan <lihan@migu.cn>
2021-10-28 14:20:51 +0800

    优化测试

--- test.js
+++ test.js
@@ -277,30 +277,34 @@ async function test8() {
     await bptree.close()
 }
 
-// 数据插入表中，并读取测试，作为测试，索引添加在AID之上
+// 数据插入表中，并读取测试，作为测试，索引添加在AID之上, 索引尚不可配置
 async function test9() {
     let tbname = "test"
     let columns = []
     col0 = new Column("AID", 0, undefined, 1, "key0")
     col1 = new Column("name", 2, 32, 0, undefined)    // 最大长度为32
+    col2 = new Column("age", 0, undefined, 0, undefined)    // 最大长度为32
 
     columns.push(col0)
     columns.push(col1)
+    columns.push(col2)
 
     let table = new Table(tbname, columns, 500)
     await table.drop()
     await table.init()
 
-    let value = [1, "I am lihan"]
+    let value = [1, "lihan", 38]
 
     await table.insert(value)
 
-    let row = await table.selectById(1) 
+    let row = await table.selectById(1)
 
-    let name = Buffer.alloc(32)
-    row.copy(name, 0, 4, 36)
-
-    winston.error(name.toString().replace(/^[\s\uFEFF\xA0\0]+|[\s\uFEFF\xA0\0]+$/g, ""))
+    let nameBuff = Buffer.alloc(32)
+    row.copy(nameBuff, 0, 4, 36)
+    let name = nameBuff.toString().replace(/^[\s\uFEFF\xA0\0]+|[\s\uFEFF\xA0\0]+$/g, "")
+    let age = row.readUInt32LE(36)
+    
+    winston.error(`##name = ${name}, age = ${age}`)
 
     await table.flush()
     await table.close()
