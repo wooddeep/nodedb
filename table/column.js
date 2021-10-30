d03040d9c73c8cd0fe36f2b91a5656d68033c337
wooddeep <523427686@qq.com>
2021-10-30 10:54:33 +0800

    #bug fix: 解决读取已存在数据库文件，加载解析的问题

--- table/column.js
+++ table/column.js
@@ -6,9 +6,9 @@ const {
 class Column {
 
     constructor(
-        name,
-        type, // 0 ~ int, 1 ~ float, 2 ~ string
-        typeAux, // 字符串长度
+        name = "",
+        type = 0, // 0 ~ int, 1 ~ float, 2 ~ string
+        typeAux = 0, // 字符串长度
         keyType = 0, // 0 ~ null, 1 ~ primary key, 2 ~ unique key, 3 ~ key
         keyName = ""
     ) {
@@ -23,7 +23,7 @@ class Column {
     }
 
     size() {
-        switch(this.type) {
+        switch (this.type) {
             case 0: return 4;
             case 1: return 4;
             case 2: return this.typeAux;
